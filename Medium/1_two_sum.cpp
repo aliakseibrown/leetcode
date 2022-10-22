ae7a9cd230cc0b9efd3b3c19e2dd264c387caf4f
Aliaksei Brown <alexeybrown2099@gmail.com>
2022-10-23 01:06:53 +0200

    commit for 1

---
+++ Medium/1_two_sum.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> two;
+        for (int i = 0; i < (nums.size() - 1); i++){
+            for(int n = i+1; n < nums.size(); n++){
+                if( i != n){
+                    if((nums[i] + nums[n]) == target){
+                        two.push_back(i);
+                        two.push_back(n);
+                        return two;
+                    }
+                }
+            }
+        }
+        return two;
+    }
+};
