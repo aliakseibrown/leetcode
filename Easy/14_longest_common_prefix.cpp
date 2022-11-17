0409a0235487be469c828773c810673411d64731
Aliaksei Brown <alexeybrown2099@gmail.com>
2022-11-18 00:01:58 +0100

    task 14:runtime:96%, memory:54%

--- Easy/14_longest_common_prefix.cpp
+++ Easy/14_longest_common_prefix.cpp
@@ -1,1 +1,18 @@
-is writing
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string final = "";
+        char letter;
+        int n = strs.size();
+        cout<<n<<endl;
+        for(int i = 0; i < strs[0].size(); i++){
+            letter = strs[0][i];
+            for(int k = 0; k < n; k++){
+                if(letter != strs[k][i]) return final;
+            }
+            final.push_back(letter);
+            cout<<i<<endl;
+        }
+        return final;
+    }
+};
