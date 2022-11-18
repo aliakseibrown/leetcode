bddd50c9f668959cfd44a2aae231fd8cb83fd8bf
Aliaksei Brown <alexeybrown2099@gmail.com>
2022-11-18 21:48:36 +0100

    task 58: runtime:100%, memory:89%

--- Easy/58_length_of_last_word.cpp
+++ Easy/58_length_of_last_word.cpp
@@ -1,3 +1,17 @@
+//O(n) 100% runtime;
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = 0;
+        for( int i = s.length()-1; i >= 0; i--){
+            if(s[i] != 32) n++;
+            if(s[i] == 32 && n != 0) break;
+        }
+        return n;
+    }
+};
+
+//using string
 class Solution {
 public:
     int lengthOfLastWord(string s) {
