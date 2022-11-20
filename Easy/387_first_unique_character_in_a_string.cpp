cbca8522fd75d5424d4983a6a45daf5012ec0d0c
Aliaksei Brown <alexeybrown2099@gmail.com>
2022-11-20 02:04:33 +0100

    task 387: runtime:68%, memory:46%

--- Easy/387_first_unique_character_in_a_string.cpp
+++ Easy/387_first_unique_character_in_a_string.cpp
@@ -3,9 +3,12 @@ public:
     int firstUniqChar(string s) {
         unordered_map<char, int>map;
         string temp;
+        int n = 0;
         for(auto m: s){
             map[m]++;
+            if(map[m] == 2) n=1; 
         }
+        if( n == 0 ) return 0;
         for(auto m: map){
             if(m.second == 1) temp.push_back(m.first);
         }
