0ae8aa0d9e5aceaeaae18cddb7584d3e990871b8
Aliaksei Brown <alexeybrown2099@gmail.com>
2022-11-22 00:11:54 +0100

    task 191: runtime:100%, memory:79%

--- Easy/191_number_of_1_bits.cpp
+++ Easy/191_number_of_1_bits.cpp
@@ -1,1 +1,10 @@
-test
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        bitset<32> bits(n);
+        int sum = 0;
+        for(auto i: bits.to_string(char(0), char(1))) {
+            if(i == 1) sum++;}
+        return sum;
+    }
+};
