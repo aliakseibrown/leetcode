79dc7647edc708e40c14f4c6a31e8061b558ac77
Aliaksei Brown <alexeybrown2099@gmail.com>
2022-11-25 16:21:49 +0100

    task 66: runtime:68%, memory:62%

---
+++ Easy/66_plus_one.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        reverse(digits.begin(), digits.end());
+        digits[0] = digits[0]+1;
+        for(int i = 0; i < digits.size(); i++){
+            if(digits[i] == 10){
+                digits[i] = 0;
+                if(i == digits.size()-1) digits.push_back(1);
+                else digits[i+1] = digits[i+1] + 1;
+            }
+        }
+        reverse(digits.begin(), digits.end());
+        return digits;
+    }
+};
