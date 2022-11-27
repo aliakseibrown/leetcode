dd5feab7481d7242d8047ca02e7e1d65c9214863
Aliaksei Brown <alexeybrown2099@gmail.com>
2022-11-27 15:23:31 +0100

    task 2085: runtime:60%, memory:82%

--- Easy/2085_count_common_words_with_one_occurrence.cpp
+++ Easy/2085_count_common_words_with_one_occurrence.cpp
@@ -1,1 +1,18 @@
-none
+class Solution {
+public:
+    int countWords(vector<string>& words1, vector<string>& words2) {
+        unordered_map<string, int>map;
+        int sum = 0;
+        for(int i = 0; i < words1.size(); i++){
+            map[words1[i]]++;
+            if(map[words1[i]] > 1) map[words1[i]] = 50;
+        }
+        for(int i = 0; i < words2.size(); i++){
+            map[words2[i]]--;
+        }
+        for(auto a: map){
+            if(a.second == 0) sum++;
+        }
+        return sum;
+    }
+};
