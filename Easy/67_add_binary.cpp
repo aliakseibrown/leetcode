99ad8a3e0f8042f0e3cbca22edaaf53e2436b029
Aliaksei Brown <alexeybrown2099@gmail.com>
2022-11-29 02:06:12 +0100

    task 67: runtime 100%, memory:34%

--- Easy/67_add_binary.cpp
+++ Easy/67_add_binary.cpp
@@ -17,3 +17,31 @@ public:
 };
 
 //does not correct with long strings
+
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int lenA = a.length()-1;
+        int lenB = b.length()-1;
+        int sum = 0;
+        string c;
+        while(lenA >= 0 || lenB >= 0||sum){
+            if(lenA>=0){
+                sum += a[lenA] - '0';
+                lenA--;
+            }
+            if(lenB>=0){
+                sum += b[lenB] - '0';
+                lenB--;
+            }
+            c+= (sum%2 + '0');
+            sum = sum / 2;
+        }
+        reverse(c.begin(), c.end());
+        return c;
+    }
+};
+//2021 = 1010 + 1011 = 10101
+
+//correct one
