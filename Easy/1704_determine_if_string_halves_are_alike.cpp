84c4b58daac397c308d04c1395050a425c3addc4
Aliaksei Brown <alexeybrown2099@gmail.com>
2022-12-02 00:18:44 +0100

    task 1704: runtime:40%, memory:31%

--- Easy/1704_determine_if_string_halves_are_alike.cpp
+++ Easy/1704_determine_if_string_halves_are_alike.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
     bool halvesAreAlike(string s) {
-        string a;
-        string b;
+        int sum=0;
         for(int i = 0; i < s.length()/2; i++){
-            if(s[i] == 'a') a.push_back(s[i]);
-            if(s[i] == 'e') a.push_back(s[i]);
-            if(s[i] == 'i') a.push_back(s[i]);
-            if(s[i] == 'o') a.push_back(s[i]);
-            if(s[i] == 'u') a.push_back(s[i]);
-
-            if(s[i] == 'A') a.push_back(s[i]+32);
-            if(s[i] == 'E') a.push_back(s[i]+32);
-            if(s[i] == 'I') a.push_back(s[i]+32);
-            if(s[i] == 'O') a.push_back(s[i]+32);
-            if(s[i] == 'U') a.push_back(s[i]+32);
+            if(s[i] == 'a' || s[i] == 'A') sum++;
+            if(s[i] == 'e' || s[i] == 'E') sum++;
+            if(s[i] == 'i' || s[i] == 'I') sum++;
+            if(s[i] == 'o' || s[i] == 'O') sum++;
+            if(s[i] == 'u' || s[i] == 'U') sum++;
         }
+        cout<<sum<<" ";
         for(int i = s.length()/2; i < s.length(); i++){
-            if(s[i] == 'a') b.push_back(s[i]);
-            if(s[i] == 'e') b.push_back(s[i]);
-            if(s[i] == 'i') b.push_back(s[i]);
-            if(s[i] == 'o') b.push_back(s[i]);
-            if(s[i] == 'u') b.push_back(s[i]);
-
-            if(s[i] == 'A') b.push_back(s[i]+32);
-            if(s[i] == 'E') b.push_back(s[i]+32);
-            if(s[i] == 'I') b.push_back(s[i]+32);
-            if(s[i] == 'O') b.push_back(s[i]+32);
-            if(s[i] == 'U') b.push_back(s[i]+32);
+            if(s[i] == 'a' || s[i] == 'A') sum--;
+            if(s[i] == 'e' || s[i] == 'E') sum--;
+            if(s[i] == 'i' || s[i] == 'I') sum--;
+            if(s[i] == 'o' || s[i] == 'O') sum--;
+            if(s[i] == 'u' || s[i] == 'U') sum--;
         }
-        if(a.length() == b.length()) return true;
-        cout<<a<<" "<< b;
+        if(sum == 0) return true;
         return false;
     }
 };
