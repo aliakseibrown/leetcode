44003ab334366090d51bfeb323d29936241c6c25
Aliaksei Brown <alexeybrown2099@gmail.com>
2022-12-06 00:45:48 +0100

    task 2190: runtime:91%, memory:27%

--- Easy/2190_most_frequent_number_following_key_in_an_array.cpp
+++ Easy/2190_most_frequent_number_following_key_in_an_array.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    int mostFrequent(vector<int>& nums, int key) {
+        map<int, int>map;
+        int max = 1;
+        for(int i = 0; i < nums.size()-1; i++){
+            if(nums[i] == key){
+                map[nums[i+1]]++;
+            }
+        }
+        for(auto m:map){
+            if(m.second >= max) {
+                max = m.second;
+                key = m.first;
+            }
+        }
+        return key;
+    }
+};
 
