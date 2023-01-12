0dfa517db17030e142b80ab4cc63efbff4b20044
Aliaksei Brown <alexeybrown2099@gmail.com>
2023-01-12 23:57:01 +0100

    task 2011: runtime:74%, memory:57%

---
+++ Easy/2011_final_value_of_variable_after_performing_operations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x = 0;
+        for(auto o:operations){
+            for(auto f:o){
+                if(f == 43){
+                    x++;
+                    break;
+                }
+                if(f == 45){
+                    x--;
+                    break;
+                }
+            }
+        }
+        return x;
+    }
+};
