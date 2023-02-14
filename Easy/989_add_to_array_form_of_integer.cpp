c495e8f920eb06a0cae176cf538fc2efc2a4fa6f
Aliaksei Brown <alexeybrown2099@gmail.com>
2023-02-14 23:25:52 +0100

    task 989: runtime:73%, memory:14%

---
+++ Easy/989_add_to_array_form_of_integer.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        int temp = 0;
+        int ext = 0;
+        vector<int> fine;
+        reverse(num.begin(), num.end());
+
+        for(int i = 0; i < num.size(); i++){
+            temp = num[i]+k%10+ext;
+            ext = 0;
+            if(temp > 9){
+                fine.push_back(temp%10);
+                ext = temp/10;
+            }else{
+                fine.push_back(temp);
+            }
+            k/=10;
+        }
+        int l = 0;
+        
+        if(k > 0){
+            while(k > 0){
+                temp = k%10+ext;
+                ext = 0;
+                if(temp > 9){
+                    fine.push_back(temp%10);
+                    ext = temp/10;
+                }else{
+                    fine.push_back(temp);
+                }
+                k/=10;
+            }
+        }
+        if(ext != 0){
+            fine.push_back(ext);
+        }
+        reverse(fine.begin(), fine.end());
+        return fine;
+    }
+};
