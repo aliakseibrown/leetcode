148efaf60075d0ca5e461a55c5ac755e2dd85124
Aliaksei Brown <alexeybrown2099@gmail.com>
2023-09-20 16:09:18 +0200

    task 19: runtime: 79%, memory: 99%

---
+++ python/medium/19_remove_nth_node_from_end_of_list.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        while head:
+            temp = ListNode()
+            temp.val = head.val
+            temp.next = prev
+            prev = temp
+            head = head.next
+        return prev
+
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> 
+Optional[ListNode]:
+        copy = head
+        s = Solution()
+        reverse = s.reverseList(copy)
+        correct_list = None
+        while reverse:
+            if n == 1: 
+                n-=1
+                reverse = reverse.next
+                continue
+            temp = ListNode()
+            temp.val = reverse.val
+            temp.next = correct_list
+            correct_list = temp
+            reverse = reverse.next
+            n-=1
+        return correct_list
