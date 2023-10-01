a6c48d60eff7f192e77599ecad68008cc9994f5e
Aliaksei Brown <alexeybrown2099@gmail.com>
2023-10-01 23:31:54 +0200

    task 572: runtime: 34%, memory: 87%

---
+++ python/easy/572_subtree_of_another_tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> 
+bool:
+        if p == None and q == None: return True
+        if p != None and q == None: return False
+        if p == None and q != None: return False
+        if p.val != q.val: return False
+        return self.isSameTree(p.left, q.left) and 
+self.isSameTree(p.right, q.right)
+
+    def isSubtree(self, root: Optional[TreeNode], subRoot: 
+Optional[TreeNode]) -> bool:
+        if not subRoot: return False
+        if not root: return False
+        if self.isSameTree(root, subRoot): return True
+        return self.isSubtree(root.left, subRoot) or 
+self.isSubtree(root.right, subRoot)      
+
