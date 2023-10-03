2722bf6986442d09bae2b2c34a24f5e4d67a51bf
Aliaksei Brown <alexeybrown2099@gmail.com>
2023-10-03 23:59:40 +0200

    task 101: runtime: 37%, memory: 65%

---
+++ python/easy/101_symmetric_tree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> 
+bool:
+        if p == None and q == None: return True
+        if p != None and q == None: return False
+        if p == None and q != None: return False
+        if p.val != q.val: return False
+        return self.isSameTree(p.left, q.right) and 
+self.isSameTree(p.right, q.left)
+    
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if root == None: return True
+        return self.isSameTree(root.left, root.right)
