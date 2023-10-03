2249b380c229692cd0a8a14acc2a9c7b002e0440
Aliaksei Brown <alexeybrown2099@gmail.com>
2023-10-04 00:02:43 +0200

    task 101: runtime: 72%, memory: 65%

--- python/easy/101_symmetric_tree.py
+++ python/easy/101_symmetric_tree.py
@@ -8,8 +8,7 @@ class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> 
 bool:
         if p == None and q == None: return True
-        if p != None and q == None: return False
-        if p == None and q != None: return False
+        if p == None or q == None: return False
         if p.val != q.val: return False
         return self.isSameTree(p.left, q.right) and 
 self.isSameTree(p.right, q.left)
