3118bab03a33921098fd13f67d5cbdacc5dd0d4a
Aliaksei Brown <alexeybrown2099@gmail.com>
2024-01-28 11:43:00 +0100

    task 682: runtime:38%, memory:60%

---
+++ python/easy/682_baseball_game.py
@@ -0,0 +1,15 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stack = []
+        score = 0
+        for i in operations:
+            if i == '+':
+                stack.append(stack[-1] + stack[-2])
+            elif i == 'D':
+                stack.append(stack[-1] * 2)
+            elif i == 'C':
+                stack.pop()
+            else:
+                stack.append(int(i))
+        return sum(stack)
+        
