79bab8f278a374567c45611b19025c30d2fa72da
Aliaksei Brown <alexeybrown2099@gmail.com>
2024-02-04 12:16:00 +0100

    task 26: runtime:35%, memory:78%

---
+++ python/easy/26_remove_dublicates_from_sorted_array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        slow, fast = 0, 1
+        for fast in range(len(nums)):
+            if nums[slow] == nums[fast]:
+                fast += 1
+            else:
+                nums[slow+1] = nums[fast]
+                fast+=1
+                slow+=1
+        return slow + 1
